631ce2d5dfe847282ba13413976c54d428c84edf
SacredMike31 <Michaelcoggins53@hotmail.com>
2026-07-03 05:27:45 +0100

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import AutoTraderPro from './AutoTraderPro.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <AutoTraderPro />
-  </React.StrictMode>
-)
